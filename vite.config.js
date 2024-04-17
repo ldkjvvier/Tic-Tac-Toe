a3ced4697bfd3424ed096e9bb3b9b6ef0ad0b513
ldkjvvier <javier.madariaga02@inacapmail.cl>
2024-04-17 18:21:31 -0400

    Update base URL for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: 'https://ldkjvvier.github.io/tic-tac-toe'
+	plugins: [react()],
+	base: 'https://ldkjvvier.github.io/Tic-Tac-Toe',
 })
