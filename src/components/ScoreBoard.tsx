2cf56f15b7a0e8ff2a79d17a78b416e8c09d71da
Javier Madariaga <145422138+ldkjvvier@users.noreply.github.com>
2024-10-25 16:20:11 -0300

    style: add icons on scoreboard

--- src/components/ScoreBoard.tsx
+++ src/components/ScoreBoard.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from 'react-redux';
-
+import { Cross, Circle } from './icons';
 interface ScoreBoardProps {
   children: React.ReactNode;
 }
@@ -19,17 +19,23 @@ export const ScoreBoard = ({ children }: ScoreBoardProps) => {
   return (
     <>
       <section className="grid grid-cols-3 justify-items-center md:text-2xl mt-12 md:max-w-[65%] ">
-        <div className="text-center">
-          <p>JUGADOR (x)</p>
-          <span>{user.Xwinner}</span>
+        <div className="text-center flex items-center">
+          <span className="w-4 h-4 inline-block">
+            <Cross />
+          </span>
+          <span className="ml-1">JUGADOR:</span>
+          <span className="ml-3">{user.Xwinner}</span>
         </div>
-        <div>
+        <div className="text-center flex flex-col items-center">
           <p>-</p>
           <span>{user.draw}</span>
         </div>
-        <div className="text-center">
-          <p>JUGADOR (O)</p>
-          <span>{user.Owinner}</span>
+        <div className="text-center flex items-center">
+          <span className="w-4 h-4 inline-block">
+            <Circle />
+          </span>
+          <span className="ml-1">JUGADOR:</span>
+          <span className="ml-3">{user.Owinner}</span>
         </div>
       </section>
       <section className="flex justify-center gap-3 mt-6">{children}</section>
